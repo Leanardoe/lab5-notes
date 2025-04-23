3757ecaefef9675a83489dfc6ea5568b639590cd
Chris moore <lninten@gmail.com>
2025-04-23 18:17:09 -0400

    added floating action button

--- app/src/main/java/com/example/notes/data/NotesRepository.kt
+++ app/src/main/java/com/example/notes/data/NotesRepository.kt
@@ -2,7 +2,7 @@ package com.example.notes.data
 
 import kotlinx.coroutines.flow.Flow
 
-class NotesRepository(private val noteDao: NoteDao) :  {
+class NotesRepository(private val noteDao: NoteDao) {
     fun getAllNotesStream(): Flow<List<Note>> = noteDao.getAllNotes()
     fun getNoteStream(id: Int): Flow<Note?> = noteDao.getNote(id)
     suspend fun insertNote(note: Note) = noteDao.insert(note)
